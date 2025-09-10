17558d133e85c889ac147af4fd1aa18e3e869854
rongshuge <shugerong@gmail.com>
2025-09-09 22:49:46 -0400

    97 finished

--- tasks/human_eval_097.rs
+++ tasks/human_eval_097.rs
@@ -12,24 +12,12 @@ verus! {
 
 spec fn unit(a: int) -> int {
     if a > 0 {
-        a%10
+        a % 10
     } else {
-        (-a)%10
+        (-a) % 10
     }
 }
 
-proof fn equal_unit_of_min (a: int)
-    requires a == i32::MIN as int,
-    ensures (-a)%10 == 10-a % 10,
-{
-
-}
-
-spec fn mul_unit(a: int, b: int) -> int {
-    let a_unit = unit(a);
-    let b_unit = unit(b);
-    a*b
-}
 
 fn abs_mod(a: i32) -> (ret: i32)
     ensures
@@ -39,32 +27,27 @@ fn abs_mod(a: i32) -> (ret: i32)
     if a >= 0 {
         a % 10
     } else if a == i32::MIN {
-        // Use residue normalization: r = ((a % 10) + 10) % 10
-        let r = 10 - a % 10;
-        proof {
-            equal_unit_of_min(a as int);
-        }
-        
-        assert(r == unit(a as int));
-        r
+        assert((10 - (a % 10)) == 8);
+        assert(unit(a as int) == 8); 
+        8
     } else {
-        // a < 0 and a != MIN, so -a is well-defined and nonnegative
         let t = (-a) % 10;
         assert(0 <= t < 10);
         t
     }
 }
 
-// proof fn mutiplication_is_monotone
+
 fn multiply(a: i32, b: i32) -> (ret: i32) 
     ensures
-        ret == mul_unit(a as int, b as int),
+        ret == unit(a as int) * unit(b as int),
 {
     let a_unit = abs_mod(a);
     let b_unit = abs_mod(b);
     if (a_unit == 0){
         return 0;
     }
+    
     assert(0 < a_unit < 10 );
     assert(0 <= b_unit < 10 );
     assert(a_unit * b_unit < a_unit * 10 ) by {
@@ -72,7 +55,6 @@ fn multiply(a: i32, b: i32) -> (ret: i32)
     }
     assert(0 <= a_unit * b_unit < 100);
     a_unit * b_unit
-    
 }
 } // verus!
 fn main() {}
