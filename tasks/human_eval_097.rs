739c715187e0dfb35a66400eeda60a839a2c3767
rongshuge <shugerong@gmail.com>
2025-09-09 22:19:50 -0400

    97 with 2 errors

--- tasks/human_eval_097.rs
+++ tasks/human_eval_097.rs
@@ -6,11 +6,74 @@ HumanEval/97
 ### VERUS BEGIN
 */
 use vstd::prelude::*;
+use vstd::arithmetic::mul::lemma_mul_left_inequality;
 
 verus! {
 
-// TODO: Put your solution (the specification, implementation, and proof) to the task here
+spec fn unit(a: int) -> int {
+    if a > 0 {
+        a%10
+    } else {
+        (-a)%10
+    }
+}
 
+proof fn equal_unit_of_min (a: int)
+    requires a == i32::MIN as int,
+    ensures (-a)%10 == 10-a % 10,
+{
+
+}
+
+spec fn mul_unit(a: int, b: int) -> int {
+    let a_unit = unit(a);
+    let b_unit = unit(b);
+    a*b
+}
+
+fn abs_mod(a: i32) -> (ret: i32)
+    ensures
+        0 <= ret < 10,
+        ret == unit(a as int),
+{
+    if a >= 0 {
+        a % 10
+    } else if a == i32::MIN {
+        // Use residue normalization: r = ((a % 10) + 10) % 10
+        let r = 10 - a % 10;
+        proof {
+            equal_unit_of_min(a as int);
+        }
+        
+        assert(r == unit(a as int));
+        r
+    } else {
+        // a < 0 and a != MIN, so -a is well-defined and nonnegative
+        let t = (-a) % 10;
+        assert(0 <= t < 10);
+        t
+    }
+}
+
+// proof fn mutiplication_is_monotone
+fn multiply(a: i32, b: i32) -> (ret: i32) 
+    ensures
+        ret == mul_unit(a as int, b as int),
+{
+    let a_unit = abs_mod(a);
+    let b_unit = abs_mod(b);
+    if (a_unit == 0){
+        return 0;
+    }
+    assert(0 < a_unit < 10 );
+    assert(0 <= b_unit < 10 );
+    assert(a_unit * b_unit < a_unit * 10 ) by {
+        lemma_mul_left_inequality(a_unit as int, b_unit as int, 10);
+    }
+    assert(0 <= a_unit * b_unit < 100);
+    a_unit * b_unit
+    
+}
 } // verus!
 fn main() {}
 
