84b62000fed5ef6cc3eda41b58871f8b113f76d3
rongshuge <shugerong@gmail.com>
2025-09-25 00:11:06 -0400

    94

--- tasks/human_eval_094.rs
+++ tasks/human_eval_094.rs
@@ -1,10 +1,3 @@
-/*
-### ID
-HumanEval/94
-*/
-/*
-### VERUS BEGIN
-*/
 use vstd::prelude::*;
 
 verus! {
@@ -45,11 +38,7 @@ fn is_prime_impl(n: u8) -> (res: bool)
     return res;
 }
 
-/* 
-This function takes lst: Vec<u8> as an input, and returns:
-a. 0, if lst is empty, or doesn't contain prime numbers
-b. the largest prime number in lst, otherwise
-*/
+
 fn max_prime(lst: Vec<u8>) -> (ret: u8) 
     ensures
         lst.len() == 0 ==> ret == 0,
@@ -61,7 +50,9 @@ fn max_prime(lst: Vec<u8>) -> (ret: u8)
                 0 <= k < lst.len() ==> !is_prime(lst[k] as nat),
         ret != 0 ==>
             forall|k| #![trigger lst[k]] 
-                0 <= k < lst.len() && is_prime(lst[k] as nat) ==> lst[k] <= ret,
+                0 <= k < lst.len() && is_prime(lst[k] as nat) ==> lst[k] <= ret
+            && is_prime(ret as nat)
+            && exists|k| 0 <= k < lst.len() && lst[k] == ret,
 {
     if lst.len() == 0 {
         return 0;
@@ -117,81 +108,3 @@ fn max_prime(lst: Vec<u8>) -> (ret: u8)
 
 
 } // verus!
-
-fn main() {}
-
-/*
-### VERUS END
-*/
-
-/*
-### PROMPT
-
-
-def skjkasdkd(lst):
-    """You are given a list of integers.
-    You need to find the largest prime value and return the sum of its digits.
-
-    Examples:
-    For lst = [0,3,2,1,3,5,7,4,5,5,5,2,181,32,4,32,3,2,32,324,4,3] the output should be 10
-    For lst = [1,0,1,8,2,4597,2,1,3,40,1,2,1,2,4,2,5,1] the output should be 25
-    For lst = [1,3,1,32,5107,34,83278,109,163,23,2323,32,30,1,9,3] the output should be 13
-    For lst = [0,724,32,71,99,32,6,0,5,91,83,0,5,6] the output should be 11
-    For lst = [0,81,12,3,1,21] the output should be 3
-    For lst = [0,8,1,2,1,7] the output should be 7
-    """
-
-*/
-
-/*
-### ENTRY POINT
-skjkasdkd
-*/
-
-/*
-### CANONICAL SOLUTION
-    def isPrime(n):
-        for i in range(2,int(n**0.5)+1):
-            if n%i==0:
-                return False
-
-        return True
-    maxx = 0
-    i = 0
-    while i < len(lst):
-        if(lst[i] > maxx and isPrime(lst[i])):
-            maxx = lst[i]
-        i+=1
-    result = sum(int(digit) for digit in str(maxx))
-    return result
-
-
-*/
-
-/*
-### TEST
-def check(candidate):
-
-    # Check some simple cases
-    assert candidate([0,3,2,1,3,5,7,4,5,5,5,2,181,32,4,32,3,2,32,324,4,3]) == 10, "This prints if this assert fails 1 (good for debugging!)"
-
-    # Check some edge cases that are easy to work out by hand.
-    assert candidate([1,0,1,8,2,4597,2,1,3,40,1,2,1,2,4,2,5,1]) == 25, "This prints if this assert fails 2 (also good for debugging!)"
-
-    # Check some edge cases that are easy to work out by hand.
-    assert candidate([1,3,1,32,5107,34,83278,109,163,23,2323,32,30,1,9,3]) == 13, "This prints if this assert fails 3 (also good for debugging!)"
-
-    # Check some edge cases that are easy to work out by hand.
-    assert candidate([0,724,32,71,99,32,6,0,5,91,83,0,5,6]) == 11, "This prints if this assert fails 4 (also good for debugging!)"
-
-    # Check some edge cases that are easy to work out by hand.
-    assert candidate([0,81,12,3,1,21]) == 3, "This prints if this assert fails 5 (also good for debugging!)"
-
-    # Check some edge cases that are easy to work out by hand.
-    assert candidate([0,8,1,2,1,7]) == 7, "This prints if this assert fails 6 (also good for debugging!)"
-
-    assert candidate([8191]) == 19, "This prints if this assert fails 7 (also good for debugging!)"
-    assert candidate([8191, 123456, 127, 7]) == 19, "This prints if this assert fails 8 (also good for debugging!)"
-    assert candidate([127, 97, 8192]) == 10, "This prints if this assert fails 9 (also good for debugging!)"
-
-*/
